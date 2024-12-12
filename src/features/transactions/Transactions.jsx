8c792e2fe3415b6d97f7e66eab67e980525ca21f
Ignacio <iggyjurado45@gmail.com>
2024-12-11 23:38:18 -0500

    its going

--- src/features/transactions/Transactions.jsx
+++ src/features/transactions/Transactions.jsx
@@ -1,6 +1,6 @@
 import { useState } from "react";
-
-import { transfer } from "./transactionsSlice";
+import { useDispatch, useSelector } from "react-redux";
+import { transfer, deposit, withdrawal } from "./transactionsSlice";
 import "./transactions.scss";
 
 /**
@@ -8,11 +8,12 @@ import "./transactions.scss";
  */
 export default function Transactions() {
   // TODO: Get the balance from the Redux store using the useSelector hook
-  const balance = 0;
+  const balance = useSelector((state) => state.transactions.balance);
 
   const [amountStr, setAmountStr] = useState("0.00");
   const [recipient, setRecipient] = useState("");
 
+  const dispatch = useDispatch();
   /** Dispatches a transaction action based on the form submission. */
   const onTransaction = (e) => {
     e.preventDefault();
@@ -24,10 +25,14 @@ export default function Transactions() {
     const amount = +amountStr;
 
     // TODO: Dispatch the appropriate transaction action based on `action`
+    // The `transfer` action is dispatched with a payload containing
+    // the amount and the recipient.
     if (action === "transfer") {
-      // The `transfer` action is dispatched with a payload containing
-      // the amount and the recipient.
       dispatch(transfer({ amount, recipient }));
+    } else if (action === "withdrawal") {
+      dispatch(withdrawal({ amount }));
+    } else if (action === "deposit") {
+      dispatch(deposit({ amount }));
     }
   };
 
